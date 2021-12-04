3c1c6bdaf10c4ffb0ff1279e4afa5a2591e4adba
mboy1011 <lyjieme@gmail.com>
2021-12-04 16:34:49 +0800

    First commit

---
+++ php/src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Docker App</title>
+</head>
+<body>
+	<h1 id="head">Welcome to Docker, GCCians!</h1>
+	<?PHP 
+		echo "PHP";
+	?>
+	<script>
+		let id = document.querySelector("#head");
+		id.innerHtml = 'JavaScript!';
+		console.log("JavaScript is working...");
+	</script>	
+</body>
+</html>
